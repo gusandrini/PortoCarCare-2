e2828f1167b4491dce5759266e29ddcf77e89da3
gusandrini <gustavosandrini92@gmail.com>
2024-09-03 19:16:36 -0300

    conteudo inicial do cabeçalho

---
+++ PortoCarCare/src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,10 @@
+import PortoCarCare from './Imagens/portocare.png'
+
+export default function Cabecalho(){
+    return(
+        <header>
+            <h1>PortoCarCare</h1>
+            <img src={PortoCarCare} width="250px"/>
+        </header>
+    );
+}
