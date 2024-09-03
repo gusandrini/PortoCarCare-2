5e1576db5cc380c274e07c8da4b8399e00114e6d
gusandrini <gustavosandrini92@gmail.com>
2024-09-03 19:17:34 -0300

    conteudo inicial da pagina

---
+++ PortoCarCare/src/App.tsx
@@ -0,0 +1,16 @@
+import Cabecalho from './components/Cabecalho/Cabecalho';
+import Rodape from './components/Rodape/Rodape';
+
+function App(){
+  return(
+    <div>
+      <Cabecalho/>
+      <Rodape/>
+    </div>
+
+
+
+  );
+}
+
+export default App;
