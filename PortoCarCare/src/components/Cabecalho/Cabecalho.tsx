513041cf0004737ff2bdf54638c5ae0e420e83f1
gusandrini <gustavosandrini92@gmail.com>
2024-09-04 11:06:49 -0300

    *correção* colocado conteudo do cabecalho e importado imagens do conteudo

--- PortoCarCare/src/components/Cabecalho/Cabecalho.tsx
+++ PortoCarCare/src/components/Cabecalho/Cabecalho.tsx
@@ -6,7 +6,28 @@ import App from '../../App';
 export default function Cabecalho(){
     return(
         <header>
-            
+            <div className="menu">
+                <nav>
+                    <a href={App}><img src={Porto} alt="logo da Porto Seguro" className='logo-topo' /></a>
+                    <ul>
+                        <li><a href="#">SOBRE NÓS</a></li>
+                        <li><a href="#">AJUDA</a></li>
+                        <li><a className="watson" href="https://web-chat.global.assistant.watson.appdomain.cloud/preview.html?region=us-south&integrationID=250f2872-90d1-4b69-a463-e7c03c0581c9&serviceInstanceID=cd14777c-39ac-401b-938f-9849d2bb018f">
+                        ASSISTENTE VIRTUAL</a></li>
+                        <li><a href="#" className="area-cli">ÁREA DO CLIENTE</a></li>
+                    </ul>
+                </nav>
+            </div>
+
+            <div className="cabecalho">
+                <h1>
+                Conte com um seguro de carro, conheça a <br /> Seguro  Auto da Porto Seguro!
+                </h1>
+                <a className='saiba-mais' href="https://www.portoseguro.com.br/seguro-auto?utm_source=google&utm_medium=cpa&utm_campaign=automovel_na_marca_se
+                arch_aon_performance_hq_growth_auto&utm_content=key_transacao_automatico_cross_marca_pesquisa_saiba-mais_nu_teste-nao_24uzq00
+                229he&ref=24uzq00229he&gad_source=1&gclid=CjwKCAjw59q2BhBOEiwAKc0ije0oTEJb6IhSgK-USQHB-55pAB6qF13TNSn_obiCI7hn7Xv5KRYsKxoCiVQQAvD_
+                BwE"><strong>Saiba mais</strong></a>
+            </div>
 
 
         </header>
--- PortoCarCare/src/components/Conteudo/Conteudo.tsx
+++ PortoCarCare/src/components/Conteudo/Conteudo.tsx
@@ -5,30 +5,7 @@ import MulherMotor from '../Imagens/mulher-motor.png'
 export default function Conteudo(){
     return (
         <main>
-            <div className="inicio">
-                <img src={PortoCarCare} alt="logo da PortoCarCare" />
-                <h1 className="texto-inicio">Clareza no<br />diagnóstico,<br />solução eficiente.</h1>
-            </div>
-
-            <div className="imagens-inicio">
-                <img src="" alt="" />
-                <img src="" alt="" />
-                <img src="" alt="" />
-            </div>
-
-            <div className="cards">
-                <div className="assistente-virtual">
-                    <img src={Calculadora} alt="calculadora" />
-                    <p>Saiba mais sobre os nossos <br/> serviços</p>
-                    <a href="#">Clique aqui</a>
-                </div>
-
-                <div className="medidas-preventivas">
-                    <img src={MulherMotor} alt="mulher mexendo no motor" />
-                    <p>Medidas preventivas</p>
-                    <a href="#">Clique aqui</a>
-                </div>
-            </div>
+            
         </main>
     );
 }
