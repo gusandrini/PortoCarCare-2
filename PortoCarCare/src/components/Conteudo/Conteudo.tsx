381ac41a4704515f848eeacae1b0b8aa6fe43825
gusandrini <gustavosandrini92@gmail.com>
2024-09-04 08:22:07 -0300

    adicionado conteudo

---
+++ PortoCarCare/src/components/Conteudo/Conteudo.tsx
@@ -0,0 +1,15 @@
+export default function Conteudo(){
+    return (
+        <main>
+            <h2>Aqui fica todo o conteúdo!!</h2>
+            <p>Este é um parágrafo de teste</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam modi reiciendis nesciunt inventore libero explicabo tempora 
+                veritatis deserunt deleniti facilis atque, ut rerum a accusantium illum esse dolores aspernatur porro delectus non? Ducimus, 
+                magni iste provident iure iusto possimus hic qui officia quam ex doloribus culpa non cumque praesentium vel.</p>
+                <div>
+                    
+                </div>
+
+        </main>
+    );
+}
