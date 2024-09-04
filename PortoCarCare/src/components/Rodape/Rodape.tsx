c3d512b4f04a5d2c2092af70d9680379941b7c2e
gusandrini <gustavosandrini92@gmail.com>
2024-09-04 08:45:56 -0300

    adicionando conteudo ao rodapé

--- PortoCarCare/src/components/Rodape/Rodape.tsx
+++ PortoCarCare/src/components/Rodape/Rodape.tsx
@@ -1,7 +1,45 @@
+import Question from '../Imagens/question.png'
+import Localizacao from '../Imagens/localizacao.png'
+import Whats from '../Imagens/whatsapp.png'
+import Telefone from '../Imagens/telefone.png'
+import Ouvidoria from '../Imagens/ouvidoria.png'
+import Exclamacao from '../Imagens/ponto-de-exclamacao.png'
+import Chat from '../Imagens/chat.png'
+
+
+
 export default function Rodape(){
     return(
-        <footer>
-            <p>Rodape</p>
+        <footer className="rodape">
+            <h2>
+            LINKS RÁPIDOS!
+            </h2>
+            <ul>
+                <li>
+                    <a href="#"><img className='linkr' src={Question}/>Perguntas
+                    frequentes</a>
+                </li>
+                <li>
+                    <a href="#"><img className='linkr' src={Localizacao}/>Endereços</a>
+                </li>
+                <li>
+                    <a href="#"><img className='linkr' src={Whats}/>WhatsApp</a>
+                </li>
+                <li>
+                    <a href="#"><img className='linkr' src={Telefone}/>SAC e telefones</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <a href="#"><img className='linkr' src={Ouvidoria}/> Ouvidoria</a>
+                </li>
+                <li>
+                    <a href="#"><img className='linkr' src={Exclamacao}/>Canal de denúncias</a>
+                </li>
+                <li>
+                    <a href="#"><img className='linkr' src={Chat}/>Chat online</a>
+                </li>
+            </ul>
         </footer>
     );
 }
