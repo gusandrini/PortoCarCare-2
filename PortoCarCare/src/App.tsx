ec277f6922f5fa65e21917f1a3ddbac87a31f98a
gusandrini <gustavosandrini92@gmail.com>
2024-09-04 08:22:55 -0300

    adicionado conteudo a pagina principal

--- PortoCarCare/src/App.tsx
+++ PortoCarCare/src/App.tsx
@@ -1,10 +1,12 @@
 import Cabecalho from './components/Cabecalho/Cabecalho';
+import Conteudo from './components/Conteudo/Conteudo';
 import Rodape from './components/Rodape/Rodape';
 
 function App(){
   return(
     <div>
       <Cabecalho/>
+        <Conteudo/>
       <Rodape/>
     </div>
 
