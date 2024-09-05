e204dc9b7b734ecda0ab50a05ad943966ac01b6f
gusandrini <gustavosandrini92@gmail.com>
2024-09-05 12:53:29 -0300

    criando o router

--- PortoCarCare/src/main.tsx
+++ PortoCarCare/src/main.tsx
@@ -1,9 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// 1 - configurando router
+import{createBrowserRouter, RouterProvider} from "react-router-dom";
+import Home from './routes/Home.tsx';
+import Contact from './routes/Contact.tsx';
+
+const router = createBrowserRouter ([
+  {
+    path: "/",
+    element: <Home/>
+  },
+  {
+    path: "contact",
+    element: <Contact/>
+  },
+])
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 )
