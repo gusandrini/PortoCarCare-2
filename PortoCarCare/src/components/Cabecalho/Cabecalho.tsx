834ed7c7beaefb17c31b6cf3fc84bf0f92f44a68
gusandrini <gustavosandrini92@gmail.com>
2024-09-06 09:11:52 -0300

    correção do conteudo

--- PortoCarCare/src/components/Cabecalho/Cabecalho.tsx
+++ PortoCarCare/src/components/Cabecalho/Cabecalho.tsx
@@ -1,9 +1,9 @@
 import { Link } from 'react-router-dom';
-import Porto from '../Imagens/porto-topo.png'
+import Porto from '../Imagens/porto-topo.png';
 
 
-function Cabecalho(){
-    return(
+function Cabecalho() {
+    return (
         <header>
             <div className="menu">
                 <nav>
@@ -12,21 +12,13 @@ function Cabecalho(){
                         <li><Link to="/sobre">SOBRE NÓS</Link></li>
                         <li><Link to="/ajuda">AJUDA</Link></li>
                         <li><a className="watson" href="https://web-chat.global.assistant.watson.appdomain.cloud/preview.html?region=us-south&integrationID=250f2872-90d1-4b69-a463-e7c03c0581c9&serviceInstanceID=cd14777c-39ac-401b-938f-9849d2bb018f">
-                        ASSISTENTE VIRTUAL</a></li>
+                            ASSISTENTE VIRTUAL</a></li>
                         <li><Link to="/area">ÁREA DO CLIENTE</Link></li>
                     </ul>
                 </nav>
             </div>
 
-            <div className="cabecalho">
-                <h1>
-                Conte com um seguro de carro, conheça a <br /> Seguro  Auto da Porto Seguro!
-                </h1>
-                <a className='saiba-mais' href="https://www.portoseguro.com.br/seguro-auto?utm_source=google&utm_medium=cpa&utm_campaign=automovel_na_marca_se
-                arch_aon_performance_hq_growth_auto&utm_content=key_transacao_automatico_cross_marca_pesquisa_saiba-mais_nu_teste-nao_24uzq00
-                229he&ref=24uzq00229he&gad_source=1&gclid=CjwKCAjw59q2BhBOEiwAKc0ije0oTEJb6IhSgK-USQHB-55pAB6qF13TNSn_obiCI7hn7Xv5KRYsKxoCiVQQAvD_
-                BwE"><strong>Saiba mais</strong></a>
-            </div>
+
         </header>
     );
 }
--- PortoCarCare/src/components/Conteudo/Conteudo.tsx
+++ PortoCarCare/src/components/Conteudo/Conteudo.tsx
@@ -1,11 +1,20 @@
-import PortoCarCare from '../Imagens/portocare.png'
-import Calculadora from '../Imagens/calculadora.png'
-import MulherMotor from '../Imagens/mulher-motor.png'
 import { Link } from 'react-router-dom';
+import Calculadora from '../Imagens/calculadora.png';
+import MulherMotor from '../Imagens/mulher-motor.png';
+import PortoCarCare from '../Imagens/portocare.png';
 
-function Conteudo(){
+function Conteudo() {
     return (
         <main>
+            <div className="cabecalho">
+                <h1>
+                    Conte com um seguro de carro, conheça a <br /> Seguro  Auto da Porto Seguro!
+                </h1>
+                <a className='saiba-mais' href="https://www.portoseguro.com.br/seguro-auto?utm_source=google&utm_medium=cpa&utm_campaign=automovel_na_marca_se
+                arch_aon_performance_hq_growth_auto&utm_content=key_transacao_automatico_cross_marca_pesquisa_saiba-mais_nu_teste-nao_24uzq00
+                229he&ref=24uzq00229he&gad_source=1&gclid=CjwKCAjw59q2BhBOEiwAKc0ije0oTEJb6IhSgK-USQHB-55pAB6qF13TNSn_obiCI7hn7Xv5KRYsKxoCiVQQAvD_
+                BwE"><strong>Saiba mais</strong></a>
+            </div>
             <div className="inicio">
                 <img src={PortoCarCare} alt="logo da PortoCarCare" />
                 <h1 className="texto-inicio">Clareza no<br />diagnóstico,<br />solução eficiente.</h1>
@@ -20,7 +29,7 @@ function Conteudo(){
             <div className="cards">
                 <div className="assistente-virtual">
                     <img src={Calculadora} alt="calculadora" />
-                    <p>Saiba mais sobre os nossos <br/> serviços</p>
+                    <p>Saiba mais sobre os nossos <br /> serviços</p>
                     <Link to="/saiba">Clique aqui</Link>
                 </div>
 
@@ -32,7 +41,7 @@ function Conteudo(){
             </div>
 
             <div className="participantes">
-                    <button><Link to="/participantes">Participantes</Link></button>
+                <button><Link to="/participantes">Participantes</Link></button>
             </div>
         </main>
     );
