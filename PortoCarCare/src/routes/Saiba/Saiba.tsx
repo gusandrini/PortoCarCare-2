0b0af8a52544f6efe02f7cc0e7999de2e6916f3c
gusandrini <gustavosandrini92@gmail.com>
2024-09-06 23:07:29 -0300

    adicionado conteúdo da página saiba

--- PortoCarCare/src/routes/Saiba/Saiba.tsx
+++ PortoCarCare/src/routes/Saiba/Saiba.tsx
@@ -12,6 +12,19 @@ const Saiba = () => {
                 <Link to="/"> Home / Saiba mais</Link>
             </div>
 
+            <div className="lista">
+                <h1>Conheça um pouco mais dos nossos serviços!</h1>
+                <ul>
+                    <p>Para utilizar nosso serviço de chatbot, não é necessário ter uma conta na IBM Cloud. Basta clicar na aba "Assistente Virtual", 
+                        onde a Beta, a assistente desenvolvida pela PortoCarCare, estará pronta para te ajudar. <br/>
+                        Ela foi projetada para guiá-lo(a) na identificação do diagnóstico mais preciso e fornecer orientações claras para que você tome a 
+                        melhor decisão na resolução de problemas do seu veículo. <br/> Este serviço foi especialmente criado para pessoas que não possuem 
+                        conhecimento técnico sobre veículos, garantindo que você se sinta seguro(a) e bem informado(a) ao enfrentar qualquer situação 
+                        relacionada ao seu veículo.</p>
+                </ul>
+    </div>
+
+
             <Rodape/>
         </div>
     );
