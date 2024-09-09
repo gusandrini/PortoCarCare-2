578a5780817770318df97b20a25dea72e7b767e6
gusandrini <gustavosandrini92@gmail.com>
2024-09-09 19:53:53 -0300

    estilização das paginas de cadastro e login

--- PortoCarCare/src/routes/CadastroLogin/Cadastro.tsx
+++ PortoCarCare/src/routes/CadastroLogin/Cadastro.tsx
@@ -12,18 +12,20 @@ const Cadastro = () => {
                 <Link to="/"> Home / Área do cliente</Link>
             </div>
 
-            <div className="container-l">
-                <span className="title-l"></span>
-                <span className="subtitle-l"></span>
-                <form>
-                    <input type="text" placeholder='Digite o nome' />
-                    <input type="email" placeholder='Digite o email' />
-                    <input type="password" placeholder='Digite a senha' />
-                    <Link to="/login"><button>Registrar</button></Link>
-                </form>
-                <Link to="/login"><p>Você tem conta? Login</p></Link>
-                    
+            <div className="principal">
+                <div className="container-l">
+                    <strong><span className="title-l">REGISTRO</span></strong>
+                    <form>
+                        <input type="text" placeholder='Digite o nome' />
+                        <input type="email" placeholder='Digite o email' />
+                        <input type="password" placeholder='Digite a senha' />
+                        <Link to="/login"><button>Registrar</button></Link>
+                    </form>
+                    <Link to="/login"><p>Você tem conta? Login</p></Link>
+                        
+                </div>
             </div>
+           
             <Rodape/>
         </div>
     );
--- PortoCarCare/src/routes/CadastroLogin/Login.tsx
+++ PortoCarCare/src/routes/CadastroLogin/Login.tsx
@@ -12,17 +12,19 @@ const Login = () => {
                 <Link to="/"> Home / Área do cliente</Link>
             </div>
 
-            <div className="container-l">
-                <span className="title-l"></span>
-                <span className="subtitle-l"></span>
-                <form>
-                    <input type="email" placeholder='Digite o email' />
-                    <input type="password" placeholder='Digite a senha' />
-                    <Link to="/"><button>Acessar</button></Link>
-                </form>
-                <Link to="/cadastro"><p>Você não tem conta? Cadastro</p></Link>
-                    
+            <div className="principal">
+                <div className="container-l">
+                <strong><span className="title-l">LOGIN</span></strong>
+                    <form>
+                        <input type="email" placeholder='Digite o email' />
+                        <input type="password" placeholder='Digite a senha' />
+                        <Link to="/"><button>Acessar</button></Link>
+                    </form>
+                    <Link to="/cadastro"><p>Você não tem conta? Cadastro</p></Link>
+                        
+                </div>
             </div>
+            
             <Rodape/>
         </div>
     );
