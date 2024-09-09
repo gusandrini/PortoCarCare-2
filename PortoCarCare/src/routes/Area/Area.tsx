84380cec680f25a828d9fc5709b78a54e6e406fd
gusandrini <gustavosandrini92@gmail.com>
2024-09-09 17:45:31 -0300

    input para senha

--- PortoCarCare/src/routes/Area/Area.tsx
+++ PortoCarCare/src/routes/Area/Area.tsx
@@ -32,10 +32,21 @@ const Area = () => {
                                 className={email !== "" ? "has-val input" : "input"}
                                 type="email"
                                 value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                
+                                onChange={(e) => setEmail(e.target.value)}  
                                 />
+                                <span className="focus-input" data-Placeholder="Email"></span>
                             </div>
+
+                            <div className="wrap-input">
+                                <input
+                                    className={password !== "" ? "has-val input" : "input"}
+                                    type="password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+                                <span className="focus-input" data-placeholder="Password"></span>
+                            </div>
+
                         </form>
                     </div>
                 </div>
