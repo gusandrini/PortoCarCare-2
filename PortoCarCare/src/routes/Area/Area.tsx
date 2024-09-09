f2bd2c214d8acf41f6dc2e9eaad03685596be826
gusandrini <gustavosandrini92@gmail.com>
2024-09-09 17:42:30 -0300

    criando input do email

--- PortoCarCare/src/routes/Area/Area.tsx
+++ PortoCarCare/src/routes/Area/Area.tsx
@@ -4,7 +4,7 @@ import Rodape from '../../components/Rodape/Rodape';
 import './Area.css'
 import { useState } from 'react';
 
-export const Area = () => {
+const Area = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
@@ -17,8 +17,37 @@ export const Area = () => {
                 <Link to="/"> Home / Área do cliente</Link>
             </div>
 
+            <div className="container">
+                <div className="container-login">
+                    <div className="wrap-login">
+                        <form className="login-form">
+                            <span className="login-form-title">Bem vindo!</span>
+
+                            {/* <span className="login-form-title">
+                                <img src="" alt="" />
+                            </span> */}
+
+                            <div className="wrap-input">
+                                <input 
+                                className={email !== "" ? "has-val input" : "input"}
+                                type="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                
+                                />
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
             <Rodape/>
         </div>
     );
 };
 
+export default Area
